d3db0550ff6248b46b0938468a8460efed522d93
Islam Tramov <islam_tramov@mail.ru>
2023-04-26 00:23:10 +0300

    JS refactirong

--- app/static/main.js
+++ app/static/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.btn').click(function(){
-        var e = document.getElementById("rk-variant");
+        var e = document.getElementById('rk-variant');
         var rk_variant_text = e.options[e.selectedIndex].text;
 
-        var text_input = document.getElementById("nodes-to-visit");
+        var text_input = document.getElementById('nodes-to-visit');
         var nodes_to_visit = text_input.value
 
         $.ajax({
@@ -13,16 +13,24 @@ $(document).ready(function(){
             data: {},
             success: function(response){
 
-                $(".result-table-head th").remove()
+                $('#result-table th').remove()
+                $('#result-table tr').remove()
+                $('#result-table td').remove()
 
-                $("#result-table-body tr").remove()
-                $("#result-table-body td").remove()
+                const table = document.getElementById('result-table')
+                var table_head = document.createElement('thead')
 
                 for(var i = 0; i < response.column_names.length; i++) {
-                    $(".result-table-head").append('<th>' + response.column_names[i] + '</th>')
-                }
+                    var th_element = document.createElement('th')
+                    var column_name_text_node = document.createTextNode(response.column_names[i])
 
-                const table_body = document.getElementById("result-table-body");
+                    th_element.appendChild(column_name_text_node)
+                    table_head.appendChild(th_element)
+                }
+                table.appendChild(table_head)
+
+                const table_body = document.createElement('tbody');
+                table.appendChild(table_body)
                 for(var i = 0; i < response.row_data.length; i++) {
                     const row = response.row_data[i];
                     var row_element = document.createElement('tr')
@@ -36,22 +44,27 @@ $(document).ready(function(){
                     table_body.appendChild(row_element)
                 }
 
-                $('.graph-image').attr('src', 'data:image/png;base64,' + response.graph_img_bytes)
 
-                $('.connectivity-components li').remove()
+                $('#graph-image').attr('src', 'data:image/png;base64,' + response.graph_img_bytes)
+
+                $('#connectivity-components li').remove()
                 for(var i = 0; i < response.node_infos.length; i++) {
                     var node_name = response.node_infos[i]['name']
                     var transitive_closure_nodes = response.node_infos[i]['transitive_closure']['nodes']
                     var reverse_transitive_closure_nodes = response.node_infos[i]['reverse_transitive_closure']['nodes']
                     var connectivity_component = response.node_infos[i]['connectivity_component']
 
-                    $('.connectivity-components').append(
-                        '<li> Вершина ' + node_name + ':<br/>' +
-                        'Г' + node_name + ': {' + transitive_closure_nodes + '}' + '<br/>' +
-                        '(Г^' + node_name + ')⁻¹' + ': {' + reverse_transitive_closure_nodes + '}' + '<br/>' +
-                        'C' + node_name + ': {' + connectivity_component + '}' + '<br/>' +
-                         '</li>'
-                    )
+                    const connectivity_components = document.getElementById('connectivity-components')
+                    const li_element = document.createElement('li')
+                    li_element.appendChild(document.createTextNode('Вершина ' + node_name))
+                    li_element.appendChild(document.createElement('br'))
+                    li_element.appendChild(document.createTextNode('Г' + node_name + ': {' + transitive_closure_nodes + '}'))
+                    li_element.appendChild(document.createElement('br'))
+                    li_element.appendChild(document.createTextNode('(Г^' + node_name + ')⁻¹: {' + reverse_transitive_closure_nodes + '}'))
+                    li_element.appendChild(document.createElement('br'))
+                    li_element.appendChild(document.createTextNode('C' + node_name + ': {' + connectivity_component + '}'))
+                    connectivity_components.appendChild(li_element)
+
                 }
             },
             error: function(ajaxContext) {
